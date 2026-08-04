4854fc3cf10b4aa0c3612168aaef1f3a2a3c407b
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-04 21:48:49 +0800

    Add files via upload

--- src/main/java/appeng/api/features/IRegistryContainer.java
+++ src/main/java/appeng/api/features/IRegistryContainer.java
@@ -79,11 +79,6 @@ public interface IRegistryContainer {
     IP2PTunnelRegistry p2pTunnel();
 
     /**
-     * get access to the ammo registry.
-     */
-    IMatterCannonAmmoRegistry matterCannon();
-
-    /**
      * get access to the player registry
      */
     IPlayerRegistry players();
